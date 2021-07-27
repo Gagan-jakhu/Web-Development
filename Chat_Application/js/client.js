4c2d4290d8c0fc8ea4d5615f1fcbea536fbee510
Gagan <gagan.bt.18@nitj.ac.in>
2021-07-27 16:46:10 +0530

    Chat Application
    This is a Real time Chat applica

---
+++ Chat_Application/js/client.js
@@ -0,0 +1,44 @@
+const socket = io.connect('http://localhost:8000');
+
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector(".container");
+// var audio = new Audio("/js/ting.mp3");
+
+const append = (message, position) => {
+
+    if (position == "left") {
+        const ganna = document.getElementById('ganna');
+
+        // console.log("play");
+        ganna.muted() = ture;
+        ganna.play();
+        ganna.muted() = ture;
+    }
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message')
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`you: ${message}`, `right`)
+    socket.emit('send', message);
+    messageInput.value = ''
+})
+const name = prompt("Enter Your name to Join");
+socket.emit('new-user-joined', name);
+
+socket.on('user-joined', name => {
+    append(`${name} joined the chat`, 'right')
+})
+socket.on('receive', data => {
+    append(`${data.name}: ${data.message}`, 'left')
+})
+socket.on('leave', name => {
+    append(`${name}: left the chat`, 'right')
+})
